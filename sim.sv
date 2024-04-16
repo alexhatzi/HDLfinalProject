f97357398b6245d8f1c7eb1ab72e861009d6f5f8
doodoofarrd <alexhatz005@gmail.com>
2024-04-16 15:44:38 -0400

    sim gen

--- sim.sv
+++ sim.sv
@@ -1,6 +1,5 @@
 `timescale 1ns / 1ps
-`include "top.v"
-
+`include "CPU.v"
 module tb_top	 	(
             output 		clk
     	 ,  output logic [31:0] inst
@@ -14,16 +13,51 @@ module tb_top	 	(
 	.result		 (result)
 			);
 
+always #1 clk = ~clk ; 
+enum reg [2:0] {"LOAD","STORE","BEQ","BLT","ADD","SUB","AND","OR"} operation; 
+
+
+assign operation = inst[31:29] ; 
+
+    class TestVector ;
+
+	rand bit [31:0] instr;
+
+	constraint opcodes {instr dist {0:=10,1:=10,2:=10,3:=10,4:=10,5:=10,6:=10,7:=10 };}
+
+    endclass
 
 
 
-initial begin
+ initial begin
+   TestVector tv = new(); 
+   $dumpvars(0, tb_top);
+   $dumpfile("dump.vcd"); 
 
-end
+   repeat(1000) begin
+   tv.randomize ; 
+   inst = tv.inst 
+   operation = inst[31:29] ; 
 
 
+
+   end
+
+
+
+
+
+
+  $finish;
+ end
 
 endmodule
+
+
+
+
+
+
 
 
 
