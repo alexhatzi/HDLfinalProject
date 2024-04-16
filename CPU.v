231bd2526bc8212e233222bbc88f40b2d3e74289
doodoofarrd <alexhatz005@gmail.com>
2024-04-16 15:18:46 -0400

    I think it technically works now

--- CPU.v
+++ CPU.v
@@ -24,26 +24,38 @@ module CPU( input clk
     
     // Instantiate decoder, Instruction Memory,
     // Data Memory, Register File and ALU
-   Decoder u_decoder ();
+   Decoder u_decoder (.inst(inst));
    InstructionMemory u_InstructionMemory ();
    DataMemory u_DataMemory ();
    RegisterFile u_RegisterFile();
    ALU u_ALU(); 
     
-    
-        
+   reg [2:0] opcode ; 
+   reg [4:0] reg_addr_0 ; 
+   reg [4:0] reg_addr_1 ; 
+   reg [4:0] reg_addr_2 ; 
+   reg [15:0] addr ;  
+   reg [31:0] inst ; 
+
+     
     always@(posedge clk)
     begin
         if(state_q == 0) begin // Fetch Stage
-            // Read instruction from instruction memory
-            // increment PC
-            // increment state
+         instruction_q =  u_InstructionMemory.read_data ;   		 // Read instruction from instruction memory
+         pc_q <=  pc_q + 1'b1 ; 						 // increment PC
+         state_q <= 2'b1 ; 					   	         // increment state
         end else if(state_q == 1) begin  // Decode Stage       
-            // Instruction Decode and read data from register/memory
-            // store all data necessary for next stages in a register 
+            inst = instruction_q ; 			// Instruction Decode and read data from register/memory
+            opcode <= u_decoder.opcode ;			// store all data necessary for next stages in a register
+	    reg_addr_0 <=u_decoder.reg_addr_0 ; 
+	    reg_addr_1 <=u_decoder.reg_addr_1 ; 
+	    reg_addr_2 <=u_decoder.reg_addr_2 ; 
+	    addr       <=u_decoder.addr	      ; 
+	     
             state_q <= 2;  //update state
         end else if(state_q == 2) begin  // Execute Stage        
             // Perform ALU operations
+	    
             state_q <= 3; //update state
         end else if(state_q == 3) begin  // Memory Stage
             // Access Memory and register file(for load)
