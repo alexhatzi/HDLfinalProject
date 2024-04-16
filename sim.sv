ebb222333a490b7ddb9b8366508bd722f7ea417e
doodoofarrd <alexhatz005@gmail.com>
2024-04-16 15:58:27 -0400

    sim changes

--- sim.sv
+++ sim.sv
@@ -1,31 +1,26 @@
 `timescale 1ns / 1ps
 `include "CPU.v"
-module tb_top	 	(
-            output 		clk
-    	 ,  output logic [31:0] inst
-         ,  input  logic [31:0] result
-			);
+module tb_top();
+ 
+ logic  clk ; 
+ logic [31:0] inst ; 
 
-
-  processor u_processor (
-	.clk		 (clk)
-	.inst		 (inst)
-	.result		 (result)
-			);
+  CPU u_CPU  (	.clk (clk));
 
 always #1 clk = ~clk ; 
-enum reg [2:0] {"LOAD","STORE","BEQ","BLT","ADD","SUB","AND","OR"} operation; 
+
+// enum {LOAD,STORE,BEQ,BLT,ADD,SUB,AND,OR} e_operation ;
 
 
-assign operation = inst[31:29] ; 
+//assign e_operation = inst[31:29] ; 
 
-    class TestVector ;
+class TestVector ;
 
 	rand bit [31:0] instr;
 
 	constraint opcodes {instr dist {0:=10,1:=10,2:=10,3:=10,4:=10,5:=10,6:=10,7:=10 };}
 
-    endclass
+endclass
 
 
 
@@ -36,26 +31,15 @@ assign operation = inst[31:29] ;
 
    repeat(1000) begin
    tv.randomize ; 
-   inst = tv.inst 
+   u_CPU.instruction_q = tv.inst 
    operation = inst[31:29] ; 
 
 
-
    end
-
-
-
-
-
-
   $finish;
  end
 
 endmodule
-
-
-
-
 
 
 
