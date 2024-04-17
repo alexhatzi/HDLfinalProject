d0b24ce459b4b343dc21fda6be2d377264b01718
doodoofarrd <alexhatz005@gmail.com>
2024-04-16 20:42:43 -0400

    systemverilog features not supported on any of the actual free tools :(

--- sim.sv
+++ sim.sv
@@ -3,41 +3,25 @@
 module tb_top();
  
  logic  clk ; 
- logic [31:0] inst ; 
 
-  CPU u_CPU  (	.clk (clk));
+ CPU u_CPU  (.clk (clk));
 
-always #1 clk = ~clk ; 
-
-// enum {LOAD,STORE,BEQ,BLT,ADD,SUB,AND,OR} e_operation ;
-
-
-//assign e_operation = inst[31:29] ; 
-
-class TestVector ;
-
-	rand bit [31:0] instr;
-
-	constraint opcodes {instr dist {0:=10,1:=10,2:=10,3:=10,4:=10,5:=10,6:=10,7:=10 };}
-
-endclass
-
+ always #1 clk = ~clk ; 
 
 
  initial begin
-   TestVector tv = new(); 
    $dumpvars(0, tb_top);
    $dumpfile("dump.vcd"); 
 
    repeat(1000) begin
-   tv.randomize ; 
-   u_CPU.instruction_q = tv.inst 
-   operation = inst[31:29] ; 
-
-
+   u_CPU.instruction_q = $urandom; 
    end
+
+
   $finish;
  end
+
+
 
 endmodule
 
